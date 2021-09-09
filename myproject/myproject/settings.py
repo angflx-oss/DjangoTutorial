790c4cae5df22c0999f4973a7ba6725168655b69
angflx-oss <angelafelix98@gmail.com>
2021-09-09 03:55:51 -0600

    Password reset first tests

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -139,3 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
